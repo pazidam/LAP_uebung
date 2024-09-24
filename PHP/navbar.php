516fa03df274b4606b0ca7db7e6cea40048327ba
pazidam <damberger.patrik@gmail.com>
2024-09-24 19:11:49 +0200

    ergebnis

--- PHP/navbar.php
+++ PHP/navbar.php
@@ -1,16 +1,16 @@
 <nav class="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
     <div class="container-fluid">
-        <a class="navbar-brand" href="?link=PRODUCT">Navbar</a>
+        <a class="navbar-brand" href="index.php?link=PRODUCT">Navbar</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="?link=PRODUCT">Produktionsfirma</a>
+                    <a class="nav-link active" aria-current="page" href="index.php?link=PRODUCT">Produktionsfirma</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="?link=CREATOR">Schauspieler</a>
+                    <a class="nav-link active" aria-current="page" href="index.php?link=CREATOR">Schauspieler</a>
                 </li>
             </ul>
         </div>
--- PHP/production.php
+++ PHP/production.php
@@ -1,38 +1,93 @@
 <?php
-?>
-<form action="production.php" method="post">
-    <div class="container pt-3">
-        <div class="row text-center">
-            <div class="col">
-                <h1>Produktionsfirma</h1>
+if(isset($_POST['search']) && !empty($_POST['search_val']))
+{
+    printProduct();
+
+    print_result($_POST['search_val']);
+
+}
+else
+{
+    printProduct();
+}
+
+function printProduct()
+{
+    ?>
+    <form action="" method="post">
+        <div class="container pt-3">
+            <div class="row text-center">
+                <div class="col">
+                    <h1>Produktionsfirma</h1>
+                </div>
             </div>
         </div>
-    </div>
-    <div class="container pt-4">
-        <div class="card shadow-sm">
-            <div class="card-header">
-                <h2>Filmsuche</h2>
-            </div>
-            <div class="card-body">
-                <div class="row pt-3">
-                    <div class="col">
-                        <div class="input-group input-group-lg">
-                            <span class="input-group-text" id="search_movie">Suche Film nach Produktionsfirma</span>
-                            <input type="text" name="search_val" class="form-control" aria-label="Sizing example input" aria-describedby="search_movie">
+        <div class="container pt-4">
+            <div class="card shadow-sm">
+                <div class="card-header">
+                    <h2>Filmsuche</h2>
+                </div>
+                <div class="card-body">
+                    <div class="row pt-3">
+                        <div class="col">
+                            <div class="input-group input-group-lg">
+                                <span class="input-group-text" id="search_movie">Suche Film nach Produktionsfirma</span>
+                                <input type="text" name="search_val" class="form-control" aria-label="Sizing example input" aria-describedby="search_movie">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row justify-content-center pt-5">
+                        <div class="col-1">
+                            <button type="submit" name="search" class="btn btn-primary btn-lg">Suchen</button>
+                        </div>
+                        <div class="col-1">
+                            <button type="submit" class="btn btn-danger btn-lg">Abbrechen</button>
                         </div>
                     </div>
                 </div>
-                <div class="row justify-content-center pt-5">
-                    <div class="col-1">
-                        <button type="submit" name="search" class="btn btn-primary btn-lg">Suchen</button>
+            </div>
+        </div>
+    </form>
+    <?php
+}
+
+function print_result($search_val)
+{
+    ?>
+    <div class="container pt-4">
+        <div class="card shadow-sm">
+            <div class="card-header">
+                <h2>Suchergebnis</h2>
+            </div>
+            <div class="card-body">
+                <div class="row pt-3">
+                    <div class="col-3">
+                        <h5>Gesuchte Produktionsfirma:</h5>
                     </div>
-                    <div class="col-1">
-                        <button type="button" class="btn btn-danger btn-lg">Abbrechen</button>
+                    <div class="col">
+                        <h5><b><?= $search_val ?></b></h5>
                     </div>
                 </div>
+                <div class="row pt-5">
+                    <div class="col-3">
+                        <h5>Gefundene Produktionsfirma:</h5>
+                    </div>
+                    <div class="col">
+                        <h5><b><?= $search_val ?></b></h5>
+                    </div>
+                </div>
+                <div class="row pt-5">
+                    <div class="col-3">
+                        <h5>Anzahl gefundener Filmtitel:</h5>
+                    </div>
+                    <div class="col">
+                        <h5><b><?= $search_val ?></b></h5>
+                    </div>
+                </div>
+
             </div>
         </div>
     </div>
-</form>
 
-
+    <?php
+}
